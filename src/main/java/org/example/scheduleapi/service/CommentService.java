98207aec7f4ad53969981a71b53f511ea3793982
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-08-01 18:15:13 +0900

    feat: 댓글 수정, 댓글 삭제

--- src/main/java/org/example/scheduleapi/service/CommentService.java
+++ src/main/java/org/example/scheduleapi/service/CommentService.java
@@ -2,6 +2,9 @@ package org.example.scheduleapi.service;
 
 import org.example.scheduleapi.dto.CommentRequestDto;
 import org.example.scheduleapi.dto.CommentResponseDto;
+import org.example.scheduleapi.dto.PasswordRequestDto;
+import org.example.scheduleapi.entity.Comment;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -9,4 +12,12 @@ public interface CommentService {
     CommentResponseDto createComment(Long scheduleId, CommentRequestDto dto);
 
     List<CommentResponseDto> getComments(Long scheduleId);
+
+    Comment findCommentOrThrow(Long id);
+
+    CommentResponseDto updateComment(Long id, CommentRequestDto dto);
+
+    void deleteComment(Long id, @RequestBody PasswordRequestDto dto);
+
+    Comment validateWithPassword(Long id, String password);
 }
--- src/main/java/org/example/scheduleapi/service/CommentServiceImpl.java
+++ src/main/java/org/example/scheduleapi/service/CommentServiceImpl.java
@@ -3,6 +3,7 @@ package org.example.scheduleapi.service;
 import lombok.RequiredArgsConstructor;
 import org.example.scheduleapi.dto.CommentRequestDto;
 import org.example.scheduleapi.dto.CommentResponseDto;
+import org.example.scheduleapi.dto.PasswordRequestDto;
 import org.example.scheduleapi.entity.Comment;
 import org.example.scheduleapi.entity.Schedule;
 import org.example.scheduleapi.repository.CommentRepository;
@@ -40,5 +41,32 @@ public class CommentServiceImpl implements CommentService {
         // 일정 찾기
         scheduleService.findScheduleOrThrow(scheduleId);
         return commentRepository.findByScheduleIdOrderByCreatedAtDesc(scheduleId).stream().map(CommentResponseDto::new).toList();
+    }
+
+    @Override
+    public Comment findCommentOrThrow(Long id) {
+        return commentRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 댓글입니다."));
+    }
+
+    @Override
+    public CommentResponseDto updateComment(Long id, CommentRequestDto dto) {
+        Comment comment = validateWithPassword(id, dto.getPassword());
+        comment.updateComment(dto.getContents(), dto.getWriter());
+        return new CommentResponseDto(comment);
+    }
+
+    @Override
+    public void deleteComment(Long id, PasswordRequestDto dto) {
+        Comment comment = validateWithPassword(id, dto.getPassword());
+        commentRepository.delete(comment);
+    }
+
+    @Override
+    public Comment validateWithPassword(Long id, String password) {
+        Comment comment = findCommentOrThrow(id);
+        if (!comment.getPassword().equals(password)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+        }
+        return comment;
     }
 }
